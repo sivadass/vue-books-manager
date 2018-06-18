80df94956d703bbbab5725fcea61490fe6c6eff7
Sivadass N <nsivadass@gmail.com>
2018-06-18 23:26:16 +0530

    Added new books details

--- src/components/BookDetails.vue
+++ src/components/BookDetails.vue
@@ -2,10 +2,24 @@
   <div class="container">
     <h1>{{ msg }}</h1>
     <ul>
+      <li>Pages: #346</li>
+      <li>Edition: May 2012</li>
+      <li>Author: Gary Stephen</li>
+      <li>Rating: 4.2/5</li>
+    </ul>
+
+    <ul>
       <li>Pages: #286</li>
       <li>Edition: May 2014</li>
       <li>Author: Susan Gray</li>
       <li>Rating: 4.5/5</li>
+    </ul>
+
+    <ul>
+      <li>Pages: #288</li>
+      <li>Edition: May 2017</li>
+      <li>Author: Brian Oberian</li>
+      <li>Rating: 4.7/5</li>
     </ul>
   </div>
 </template>
